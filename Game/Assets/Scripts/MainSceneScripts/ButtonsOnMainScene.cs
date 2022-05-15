4d64d05cbe5b4e7a06bef20210b704e852ae60c2
Serhii Karpenko <80330854+SerhiiKarpenko@users.noreply.github.com>
2022-05-16 00:44:35 +0200

    Scripts update
     ButtonsOnMainScene updated, now if game paused music is not playing

--- Game/Assets/Scripts/MainSceneScripts/ButtonsOnMainScene.cs
+++ Game/Assets/Scripts/MainSceneScripts/ButtonsOnMainScene.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 
 public class ButtonsOnMainScene : MonoBehaviour
 {
     [SerializeField] private GameObject _pauseMenu;
     [SerializeField] private GameObject _playerUI;
+    [SerializeField] private AudioMixer _audioMixer;
     private bool _isPaused = false;
+    private float _volume;
+
+    private void Start()
+    {
+        _volume = GetAudioMixerVolume();
+    }
 
     public void Resume()
     {
@@ -16,6 +24,7 @@ public class ButtonsOnMainScene : MonoBehaviour
         _playerUI.SetActive(true);
         Time.timeScale = 1f;
         _isPaused = false;
+        SetAudioMixerVolume(_volume, _audioMixer);
     }
 
     public void Pause()
@@ -24,6 +33,7 @@ public class ButtonsOnMainScene : MonoBehaviour
         _playerUI.SetActive(false);
         _isPaused = true;
         Time.timeScale = 0f;
+        SetAudioMixerVolume(-80f, _audioMixer);
     }
 
     public void LoadMenu()
@@ -31,6 +41,21 @@ public class ButtonsOnMainScene : MonoBehaviour
         Events.killedEnemiesCount = 0;
         SceneManager.LoadScene("Main_menu");
         Time.timeScale = 1f;
+    }
+
+    private void SetAudioMixerVolume(float volume, AudioMixer mixer)
+    {
+        mixer.SetFloat("volume", volume);
+    }
+
+    private float GetAudioMixerVolume()
+    {
+        float volume;
+        bool result = _audioMixer.GetFloat("volume", out volume);
+        if (result)
+            return volume;
+        else
+            return 0f;
     }
 
 
